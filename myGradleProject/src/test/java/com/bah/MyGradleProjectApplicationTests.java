dc0556688154bb29d32730e2ff26a1d9feb840a7
user <user@this.com>
2019-11-05 15:38:56 -0500

    added Gradle build

---
+++ myGradleProject/src/test/java/com/bah/MyGradleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGradleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
